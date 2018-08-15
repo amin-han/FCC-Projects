f027aca09c9c9044513ed030179b98b27f391cf7
Amin <9434966+minobino@users.noreply.github.com>
2018-08-15 09:46:40 -0400

    Update palindrome.js

--- JS Algorithms and Data Structures/Palindrome Checker/palindrome.js
+++ JS Algorithms and Data Structures/Palindrome Checker/palindrome.js
@@ -1,1 +1,19 @@
 
+function palindrome(str) {
+  
+  var x = /[^A-Za-z0-9]/g;
+  
+  str = str.toLowerCase().replace(x,'');
+
+  for (var i = 0; i < (str.length/2); i++){
+    if (str[i] !== str[(str.length) - 1 - i]){
+      return false;
+    }
+  }
+  return true;
+
+} 
+  
+
+
+palindrome("eye");
