243ec6dbdf73245d2174ad56ce0a4b610a9423ee
Amin <9434966+minobino@users.noreply.github.com>
2018-08-15 09:48:14 -0400

    Update romanum.js

--- JS Algorithms and Data Structures/Roman Numeral Converter/romanum.js
+++ JS Algorithms and Data Structures/Roman Numeral Converter/romanum.js
@@ -1,1 +1,19 @@
 
+function convertToRoman(num) {
+  var romanNum = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  var deciNum = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  
+  var conversion = '';
+  
+  for (var i = 0; i < deciNum.length; i++){
+    while (deciNum[i] <= num){
+      conversion += romanNum[i];
+      num -= deciNum[i];
+    }
+  }
+  
+  
+ return conversion;
+}
+
+convertToRoman(36);
